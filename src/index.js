226d8043f6c9704aa18d023e0391584aa4d05445
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-19 15:08:34 -0300

    Added Routes and GlobalStyle to be render in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Routes from './routes'
+import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routes />
+    <GlobalStyle />
   </React.StrictMode>,
   document.getElementById('root')
 );
