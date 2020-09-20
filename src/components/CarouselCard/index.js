c25ed46239d56c067f4f5b0a0c70a0f6b0867308
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 20:14:19 -0300

    Updated navigation after click on button

--- src/components/CarouselCard/index.js
+++ src/components/CarouselCard/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { Container, Buttons, Button } from './styles'
 
 function CarouselCard({ id, name, price, description, image }) {
+  const history = useHistory()
+
   return (
     <Container>
-      <img src={image} alt="T-shirt mock" />
+      <img src={image} alt={name} />
       <span>{name}</span>
-      <span>{price}</span>
+      <span>R$ {price}</span>
       <p>{description}</p>
       <Buttons>
         <Button backgroundColor='#2297ea'>
           Adicionar ao carrinho
         </Button>
-        <Button backgroundColor='#fc5e2f'>
+        <Button backgroundColor='#fc5e2f' onClick={() => history.push(`/${id}/productdescription`)}>
           Ver detalhes
         </Button>
       </Buttons>
