a8bb4b73232e42cd1d71bb4edd2e868aa422dd12
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 16:03:02 -0300

    Updated routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,22 @@ import { BrowserRouter, Switch } from 'react-router-dom'
 import Route from './routes'
 
 import WelcomePage from '../pages/WelcomePage'
+import SignIn from '../pages/SignIn'
+import ProductList from '../pages/ProductList'
+import ProductDescription from '../pages/ProductDescription'
+import MyCart from '../pages/Cart'
+import NotFound from '../pages/NotFound'
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={WelcomePage} isPrivate />
-
-
-        {/* <Route path="/private" component={privatePage} /> */}
+        <Route path="/" exact component={WelcomePage} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/productlist" component={ProductList} />
+        <Route path="/productdescription" component={ProductDescription} />
+        <Route path="/mycart" component={MyCart} />
+        <Route path='*' component={NotFound} />
       </Switch>
     </BrowserRouter>
   )
