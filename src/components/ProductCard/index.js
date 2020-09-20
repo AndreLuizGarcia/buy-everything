3814ce7647f4d771530e37ee26854dc2791cadf7
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 20:14:33 -0300

    Updated navigation after click on button

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,29 +1,25 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-import tShirtMock from '../../assets/t-shirt-mock.jpg'
 import { Container, Button } from './styles'
 
-function ProductCard() {
+function ProductCard({ id, name, price, description, image }) {
+  const history = useHistory()
+
   return (
     <Container>
-      <img src={tShirtMock} alt="T-shirt mock" />
-      <div>
-        <span>Mens Casual Premium Slim Fit T-Shirts</span>
-        <p>        
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+      <img src={image} alt={name} />
+      <div style={{ flex: 1 }}>
+        <span>{name}</span>
+        <p>{description}</p>
       </div>
 
       <div>
-        <span>R$ 22,30</span>
+        <span>R$ {price}</span>
         <Button backgroundColor='#2297ea'>
           Adicionar ao carrinho
         </Button>
-        <Button backgroundColor='#fc5e2f'>
+        <Button backgroundColor='#fc5e2f' onClick={() => history.push(`/${id}/productdescription`)}>
           Ver detalhes
         </Button>
       </div>
