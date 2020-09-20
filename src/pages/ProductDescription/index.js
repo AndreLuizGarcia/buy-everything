4ef7636c3ea8938c37eea074d94b50c855d49fa7
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 20:15:17 -0300

    Implemented getProducts method

--- src/pages/ProductDescription/index.js
+++ src/pages/ProductDescription/index.js
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import Header from '../../components/Header'
+import ShimmerDescriptionCard from '../../components/ShimmerDescriptionCard'
 
-import tShirtMock from '../../assets/t-shirt-mock.jpg'
+import FakeStoreAPI from '../../services/FakeStoreAPI'
 import { Container, Main, CTA, Control, AddCart } from './styles'
 
 function ProductDescription() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null)
+
   useEffect(() => {
+    getProduct()
+
     window.scrollTo(0, 0)
 
     document.body.scrollTop = 0; // For Safari
@@ -19,36 +26,47 @@ function ProductDescription() {
     })
   }, [])
 
+  async function getProduct() {
+    try {
+      const { data } = await FakeStoreAPI.get(`/products/${id}`)
+      setProduct(data)
+    } catch (error) {
+      alert('Não foi possível carregar as informações do produto. Verifique sua conexão com a internet')
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Main>
-        <h1 data-aos="fade-up">Mens Casual Premium Slim Fit T-Shirts</h1>
-        <div data-aos="fade-right">
-          <img src={tShirtMock} alt="T-shirt mock" />
-          <div>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <CTA>
-              <span>R$ 22,30</span>
-              <Control>
-                <button>-</button>
-                  <span>3</span>
-                <button>+</button>
-              </Control>
-              <AddCart>
-                Adicionar ao carrinho
-                <span>R$66,90</span>
-              </AddCart>
-            </CTA>
-          </div>
-        </div>
-      </Main>
+        {
+          product === null ? <ShimmerDescriptionCard /> :
+          <>
+            <h1 data-aos="fade-up">{product.title}</h1>
+            <div data-aos="fade-right">
+              <img src={product.image} alt={product.title} />
+              <div style={{ display: "flex",  flexDirection: "column", justifyContent: "space-around" }}>
+                <p>{product.description}</p>
+                <CTA>
+                  <span>R$ {product.price}</span>
+                  <Control>
+                    <button>-</button>
+                      <span>3</span>
+                    <button>+</button>
+                  </Control>
+                  <AddCart>
+                    Adicionar ao carrinho
+                    <span>R$66,90</span>
+                  </AddCart>
+                </CTA>
+              </div>
+            </div>
+          </>
+        }
+        </Main>
+
+
+
     </Container>
   )
 }
