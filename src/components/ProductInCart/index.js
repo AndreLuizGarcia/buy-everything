27d6e646f30c108c16d0dec130a30eff9f704bd1
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 22:44:15 -0300

    Updated component to use real values

--- src/components/ProductInCart/index.js
+++ src/components/ProductInCart/index.js
@@ -1,21 +1,14 @@
 import React from 'react'
 
-import tShirtMock from '../../assets/t-shirt-mock.jpg'
 import { Container, Control, DeleteFromCart } from './styles'
 
-function ProductInCart() {
+function ProductInCart({ id, name, price, description, image }) {
   return (
     <Container>
-      <img src={tShirtMock} alt="T-shirt mock" />
+      <img src={image} alt={name} />
       <div>
-        <span>Mens Casual Premium Slim Fit T-Shirts</span>
-        <p>        
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <span>{name}</span>
+        <p>{description}</p>
       </div>
 
       <div>
@@ -25,7 +18,7 @@ function ProductInCart() {
           <button>+</button>
         </Control>
         <DeleteFromCart>Remover do carrinho</DeleteFromCart>
-        <span>R$ 22,30</span>
+        <span>R$ {price}</span>
       </div>
     </Container>
   )
