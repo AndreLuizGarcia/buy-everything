cc2a7be022ce1cddd603b09a1b9efe8e8c6fa652
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 22:43:43 -0300

    Added redux dispatch to communicate with store

--- src/components/CarouselCard/index.js
+++ src/components/CarouselCard/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { actions as actionsCart } from '../../store/actions/cart'
 
 import { Container, Buttons, Button } from './styles'
 
 function CarouselCard({ id, name, price, description, image }) {
+  const dispatch = useDispatch()
   const history = useHistory()
+
+  const product = {
+    id,
+    name,
+    price,
+    description,
+    image
+  }
+
+  function handleAddProduct() {
+    dispatch(actionsCart.addProduct(product))
+  }
 
   return (
     <Container>
@@ -13,7 +29,7 @@ function CarouselCard({ id, name, price, description, image }) {
       <span>R$ {price}</span>
       <p>{description}</p>
       <Buttons>
-        <Button backgroundColor='#2297ea'>
+        <Button backgroundColor='#2297ea' onClick={handleAddProduct}>
           Adicionar ao carrinho
         </Button>
         <Button backgroundColor='#fc5e2f' onClick={() => history.push(`/${id}/productdescription`)}>
