1869ae95fc7228241f109aa428386badafc1f266
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-21 16:59:34 -0300

    Updated carts reducer

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,22 +1,19 @@
 import { types } from '../types/cart'
 
-const cart = (state = [], action) => {
+const initialState = []
+
+const cart = (state = initialState, action) => {
   switch (action.type) {
     case types.CART_ADD_PRODUCT:
-      return [
-        ...state,
-        action.payload
-      ]
+      return state.findIndex(item => item.id === action.payload.id) === -1 ? 
+        state = [...state, action.payload] : 
+        state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity + action.payload.quantity } : item)
     case types.CART_DECREASE_PRODUCT:
-      let product = state.indexOf(action.payload.id)
-      if (product >= 0) {
-        return [ ...state.splice(product, 1)]
-      }
-      break;
+      return state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item)
     case types.CART_REMOVE_PRODUCT:
-      return [
-        ...state.filter(product => product.id !== action.payload.id)
-      ]  
+      return state.filter(product => product.id !== action.payload.id)
+    case types.CART_REMOVE_ALL:
+      return state = []
     default:
       return [...state]
   }
