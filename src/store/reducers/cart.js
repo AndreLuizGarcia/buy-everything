7cbc78c609c34949b80a9e2b420f75faf83e0b99
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-21 17:57:49 -0300

    Persisting data in localStorage

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,19 +1,35 @@
 import { types } from '../types/cart'
 
-const initialState = []
+const initialState = localStorage.getItem('@BUY_EVERYTHING') ? JSON.parse(localStorage.getItem('@BUY_EVERYTHING')) : null
+let newState = []
 
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case types.CART_ADD_PRODUCT:
-      return state.findIndex(item => item.id === action.payload.id) === -1 ? 
-        state = [...state, action.payload] : 
-        state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity + action.payload.quantity } : item)
+      newState = state.findIndex(item => item.id === action.payload.id) === -1 ? 
+      state = [...state, action.payload] : 
+      state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity + action.payload.quantity } : item)
+
+      localStorage.setItem('@BUY_EVERYTHING', JSON.stringify(newState))
+
+      return state = newState
     case types.CART_DECREASE_PRODUCT:
-      return state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item)
+      newState = state.map(item => item.id === action.payload.id ? { ...item, quantity: item.quantity - 1 } : item)
+
+      localStorage.setItem('@BUY_EVERYTHING', JSON.stringify(newState))
+
+      return state = newState
     case types.CART_REMOVE_PRODUCT:
-      return state.filter(product => product.id !== action.payload.id)
+      newState = state.filter(product => product.id !== action.payload.id)
+
+      localStorage.setItem('@BUY_EVERYTHING', JSON.stringify(newState))
+
+      return state = newState
     case types.CART_REMOVE_ALL:
-      return state = []
+      newState = []
+      localStorage.setItem('@BUY_EVERYTHING', JSON.stringify(newState))
+
+      return state = newState
     default:
       return [...state]
   }
