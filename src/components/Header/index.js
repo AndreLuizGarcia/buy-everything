82ebd3efb11f01c2ee31016cca47d17720330ba7
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 22:44:00 -0300

    Connected with redux store

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 
+import { actions as actionsLogin } from '../../store/actions/isLogged'
 import logo from '../../assets/logo.png'
 import cart from '../../assets/cart.png'
 import { Container, LeftSide, CartButton, Button } from './styles'
 
 function Header() {
-  const isLogged = false
+  const isLogged = useSelector(state => state.isLogged)
   const history = useHistory()
+  const dispatch = useDispatch()
+  const products = useSelector(state => state.cart)
+
+  function handleLogout() {
+    dispatch(actionsLogin.isLoggedFalse(false))
+    history.push('/')
+  }
 
   return (
     <Container>
@@ -15,11 +24,11 @@ function Header() {
       { isLogged ?
         <LeftSide>
           <span>Olá, <strong>Cliente</strong></span>
-          <CartButton>
+          <CartButton onClick={() => history.push('mycart')}>
             <img src={cart} alt="Cart" />
-            <span>5</span>
+            <span>{products ? products.length : 0}</span>
           </CartButton>
-          <Button onClick={() => history.push('/logout')}>
+          <Button onClick={handleLogout}>
             SAIR
           </Button>
         </LeftSide> : <Button onClick={() => history.push('/signin')}>ENTRAR</Button>
