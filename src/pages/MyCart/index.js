0209c3565b3b42b5fcc082706ffc6948e5eec297
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-21 17:01:51 -0300

    Configured axios to cancel request if the user change page

--- src/pages/MyCart/index.js
+++ src/pages/MyCart/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+
+import { actions as actionsCart } from '../../store/actions/cart'
 
 import Header from '../../components/Header'
 import ProductInCart from '../../components/ProductInCart'
@@ -9,17 +12,31 @@ import ProductInCart from '../../components/ProductInCart'
 import { Container, Main, Footer } from './styles'
 
 function Cart() {
+  const isLogged = useSelector(state => state.isLogged)
   const products = useSelector(state => state.cart)
+  const dispatch = useDispatch()
+  const history = useHistory()
+
   useEffect(() => {
     window.scrollTo(0, 0)
-
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome,
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
 
     AOS.init({
       duration : 2000
     })
   }, [])
+
+  function handleSendCart() {
+    if(!isLogged) {
+      alert('Para concluir seu pedido é necessário entrar na sua conta.')
+      history.push('/signin')
+    } else {
+      alert('Compra realizada com sucesso!')
+      dispatch(actionsCart.removeAll())
+    }
+
+  }
 
   return (
     <Container>
@@ -30,14 +47,15 @@ function Cart() {
           <div data-aos="fade-right">
             {
               products.length === 0 ? <h1 style={{flex: 1}}>Não há itens no carrinho</h1> :
-              products.map((product, index) => (
+              products.map((product) => (
                 <ProductInCart
-                  key={product.id + index}
+                  key={product.id + Math.random()}
                   id={product.id}
                   name={product.title}
                   price={product.price}
                   description={product.description}
                   image={product.image}
+                  quantity={product.quantity}
                 />
               ))
             }
@@ -47,7 +65,7 @@ function Cart() {
       {
         products.length !== 0 ? 
         <Footer>
-          <button>Enviar pedido</button>
+          <button onClick={handleSendCart}>Enviar pedido</button>
         </Footer> : null
       }
 
