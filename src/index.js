51cb67c3c38ab1f9e6c0c8021082872b2714ad27
Andre Garcia <Andre.Garcia@la.logicalis.com>
2020-09-20 22:42:38 -0300

    Added redux provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Routes from './routes'
+import { Provider } from 'react-redux'
+import { store } from './store/store';
 import GlobalStyle from './styles/globalStyle'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
-    <GlobalStyle />
+    <Provider store={store}>
+      <Routes />
+      <GlobalStyle />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
